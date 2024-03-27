dcd381fcac5e2725b88604019cc2a321577544f8
shivam sharma <shivamsharma0887@gmail.com>
2024-03-28 00:47:17 +0530

    first commit

---
+++ components/globals/navbar.tsx
@@ -0,0 +1,67 @@
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { UserButton, currentUser } from "@clerk/nextjs";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+  const user = await currentUser();
+
+  return (
+    <header
+      className="fixed right-0 left-0 top-0 p-4 bg-black/40 z-[100]
+    flex items-center justify-between border-b-[1px] border-neutral-900
+    backdrop-blur-lg"
+    >
+      <aside className="flex items-center gap-[2px] relative">
+        <p className="text-lg sm:text-3xl font-bold">S</p>
+        <div className="absolute top-2.5 sm:top-3 left-2 sm:left-3 w-4 sm:w-6 aspect-square">
+          <Image src="/logo.png" fill alt="logo" className="object-contain" />
+        </div>
+        <p className="text-lg sm:text-3xl font-bold ml-2 sm:ml-3">napse</p>
+      </aside>
+
+      <nav className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="">Products</Link>
+          </li>
+          <li>
+            <Link href="">Pricing</Link>
+          </li>
+          <li>
+            <Link href="">Clients</Link>
+          </li>
+          <li>
+            <Link href="">Resources</Link>
+          </li>
+          <li>
+            <Link href="">Documentation</Link>
+          </li>
+          <li>
+            <Link href="">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px]
+         focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+         focus:ring-offset-slate-400"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {user ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
+        {user ? <UserButton afterSignOutUrl="/" /> : null}
+        <MenuIcon className="md:hidden" />
+      </aside>
+    </header>
+  );
+};
+
+export default Navbar;
